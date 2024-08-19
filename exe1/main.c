0f019f084367fd3973b1a6f9b721f991584e3eba
Vinícius "Mumu" Rodrigues <116031133+mumuvrf@users.noreply.github.com>
2024-08-18 23:47:26 -0300

    exe1 solved

--- exe1/main.c
+++ exe1/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 const int LED_PIN_R = 5;
+const int LED_PIN_Y = 28;
 
 int main() {
   stdio_init_all();
@@ -10,11 +11,16 @@ int main() {
   gpio_init(LED_PIN_R);
   gpio_set_dir(LED_PIN_R, GPIO_OUT);
 
+  gpio_init(LED_PIN_Y);
+  gpio_set_dir(LED_PIN_Y, GPIO_OUT);
+
   while (true) {
     gpio_put(LED_PIN_R, 1);
+    gpio_put(LED_PIN_Y, 1);
     sleep_ms(250);
 
     gpio_put(LED_PIN_R, 0);
+    gpio_put(LED_PIN_Y, 0);
     sleep_ms(250);
   }
 }
