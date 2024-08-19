56458e2ee5db114d2298be6276b6e66226fcf7a4
Vinícius "Mumu" Rodrigues <116031133+mumuvrf@users.noreply.github.com>
2024-08-18 23:47:39 -0300

    exe2 solved

--- exe2/main.c
+++ exe2/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 const int BTN_PIN = 26;
+const int SCND_BTN_PIN = 7;
 
 int main() {
   stdio_init_all();
@@ -11,10 +12,20 @@ int main() {
   gpio_set_dir(BTN_PIN, GPIO_IN);
   gpio_pull_up(BTN_PIN);
 
+  gpio_init(SCND_BTN_PIN);
+  gpio_set_dir(SCND_BTN_PIN, GPIO_IN);
+  gpio_pull_up(SCND_BTN_PIN);
+
   while (true) {
     if (!gpio_get(BTN_PIN)) {
       printf("Botao 1\n");
+      sleep_ms(250);
       while (!gpio_get(BTN_PIN)) {
+      };
+    } else if(!gpio_get(SCND_BTN_PIN)){
+      printf("Botao 2\n");
+      sleep_ms(250);
+      while (!gpio_get(SCND_BTN_PIN)) {
       };
     }
   }
